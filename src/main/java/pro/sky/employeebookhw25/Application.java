d0c0ae3d22b65bfd13af252419941dcc64ecb71f
Andreykhrs <andrey_khrs@mail.ru>
2024-04-25 18:08:56 +0800

    Initial commit!

---
+++ src/main/java/pro/sky/employeebookhw25/Application.java
@@ -0,0 +1,15 @@
+package pro.sky.employeebookhw25;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+
+
+	}
+
+}
